a93815a7c5e13c2ae07fd8501aa73b3091979c1d
sanmaru <moni.sanmaru@gmail.com>
2021-02-09 16:24:57 +0900

    initialize

---
+++ pages/amplify-login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from '../src/aws-exports';
+Amplify.configure(awsconfig);
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+const App = () => (
+  <div>
+    <AmplifySignOut />
+    My App
+  </div>
+);
+
+export default withAuthenticator(App);
