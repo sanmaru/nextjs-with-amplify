58b8b2f748327a265805b45fa4bdf6843948c61a
archrua <moni@chosunbiz.com>
2021-03-19 15:49:18 +0900

    하위 메뉴의 존재를 확인하여 메뉴를 구성할 수 있도록 수정 (Modified to be able to compose a menu by checking the existence of a sub-menu)

--- components/features/hambug/menuObject.tsx
+++ components/features/hambug/menuObject.tsx
@@ -10,15 +10,12 @@ type Props = {
 
 export default function hambugObject({ showMenu = false, userGroup = [] }: Props) {
 
-  const createLiTag = (menuArray: any[], authorityGroup: any[] = ['']) => {
-    console.log('menuSize', menuArray.length);
-    console.log('authorityGroup', authorityGroup);
+  const createLiTag = (menuArray: any[], authorityGroup: any[] = [''], parentIdx:number = 0) => {
 
-    return menuArray.map((current, index) => {
-      let liTag: any, aTag: any;
-      console.log('current', current.authGroup);
-      console.log('userGroup', userGroup);
+    return menuArray.map( (current, index) => {
+      let liTag: any, aTag: any, addSubMenu: any;
       let showFlag: boolean = false;
+
       for (let i = 0; i < userGroup.length; i++) {
         for (let j = 0; j < current.authGroup.length; j++) {
           if (userGroup[i] === current.authGroup[j]) showFlag = true;
@@ -26,22 +23,29 @@ export default function hambugObject({ showMenu = false, userGroup = [] }: Props
       }
 
       if (showFlag) {
-        // console.log('userGroup', userGroup);
-        // console.log('current', current.authGroup);
-        aTag = React.createElement('a', { key: 'menuLink' + index, href: current.link }, current.title)
-        liTag = React.createElement('li', { key: 'menu' + index }, aTag);
+        aTag = React.createElement('a', { key: 'menuLink' + '-' + index, href: current.link }, current.title);
+        liTag = React.createElement('li', { key: 'menu' + '-' + index }, aTag);
+        
+        if( current.submenu ){
+          addSubMenu = createUlTag(current.submenu, authorityGroup, parentIdx);
+          liTag = React.createElement('li', { key: 'menu' + index }, [aTag, addSubMenu]);
+        }else{
+          liTag = React.createElement('li', { key: 'menu' + index }, aTag);
+        }
       }
       return liTag;
-    });;
+    });
   }
-
-  let ulTag = React.createElement('ul', {}, createLiTag(menuList, userGroup));
+  
+  const createUlTag = (menuArray: any[], authorityGroup: any[] = [''], idx:number = 0) => {
+    return React.createElement('ul', { key:'menu' + idx + '-'}, createLiTag(menuArray, authorityGroup, idx++));
+  }
+  
+  let ulTag = createUlTag(menuList, userGroup);
 
   let classDiv = showMenu ?
-    (React.createElement('div', { className: 'hambugDiv menuList menuList-tooggle' }, ulTag))
-    : (React.createElement('div', { className: 'hambugDiv menuList' }));
-
-  console.log(classDiv);
+    (React.createElement('div', { className: 'menuList menuList-tooggle' }, ulTag))
+    : (React.createElement('div', { className: 'menuList' }));
 
   return (
     <Fragment>
