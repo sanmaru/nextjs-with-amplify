97ba4df2bff2bf065f1f711304315358410b8b37
sanmaru <moni.sanmaru@gmail.com>
2021-04-12 18:20:46 +0900

    로그인 한지 1시간이 지났을 경우 재 접속하도록 함

--- components/features/body/default.tsx
+++ components/features/body/default.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode, Fragment } from 'react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import jwt_decode from "jwt-decode";
+import { Auth } from 'aws-amplify';
 
 import DefaultAuthenticator from '../authenticator';
 import DefaultNavigator from '../navigator'
@@ -16,20 +18,38 @@ function DefaultBody({ children }: Props) {
   React.useEffect(() => {
     onAuthUIStateChange((nextAuthState, authData) => {
       setAuthState(nextAuthState);
-      setUser(authData)
+      setUser(authData);
     });
     
   }, []);
 
+  function isLogin(authState:any, AuthState: any, user:any){
+    if(user){
+      let arr = Object.entries(user.pool.storage);
+      arr.forEach( (element) => {
+        if(element[0].indexOf('accessToken') > -1) {
+          let obj = JSON.parse(JSON.stringify( jwt_decode(new String(element[1]).toString()) ));
+          if(obj.iat > obj.auth_time + (1*60*6)) {
+            console.log(false);
+            Auth.signOut({global:true});
+          }
+        }
+      });
+
+    }
+
+    return authState === AuthState.SignedIn && user;
+  }
   function getUserName( obj:any ){
     try{
+      console.log(obj.attributes.email);
       return obj.attributes.email?.substring(0, obj.attributes.email?.indexOf('@'));
     }catch(e){
       return e.message();
     }
   }
 
-  return authState === AuthState.SignedIn && user ?
+  return isLogin(authState, AuthState, user) ?
     (
       <Fragment>
         <DefaultNavigator userName={getUserName(user)} profile={user} >
